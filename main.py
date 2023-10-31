9e89ed6bd2e18e3103772fbecbd51dfb14a9f93f
MetalStar <wilsonis2017@gmail.com>
2023-10-31 10:38:12 -0400

    I created the functions:  encode_password, decode_password(), and menu(). I used for loops to access characters in the string and add them to a new string in the encoded password after 3 is added to them. I ran into a problem, I had to convert the string to an int and back to a string again so it could be concatenated. I also had to use modulo division with 10 in encode and decoding functions, so the added value did not exceed 9. For decode, I did the opposite and subtracted three for every character, and added it to a new string. The issue was for smaller numbers, the result was negative; I fixed that by doing modulo division also. I used three iff statements for th menu, and utilized the encode and decode functions appropriately, and used a while loop that ran if the user did not select “3.” I did this all in one go, but should have made smaller updates to github… All I need to do next is access my partner’s decode function whenever he finishes it

--- main.py
+++ main.py
@@ -4,5 +4,55 @@
 # get add
 # get commit "explain what you did"
 # get push
-# #this will update the versions.
-print("HELLO") # get add .
+# this will update the versions.
+
+def encode_password(user_password):
+    # set encoded password blank
+    encoded_password = ""
+    for digit in str(user_password):
+        # add 3 to the digit but if greater than 6 we have to make seperate rules
+        encoded_digit = str((int(digit) + 3) % 10)  # use modulo division to keep it less than or equal to 9
+        # add the new digit into the new encoded password
+        encoded_password += encoded_digit
+    return encoded_password
+
+
+def decode_password(encoded):
+    OG_password = ""
+    for digit in str(encoded):
+        # subtract 3 from the digit
+        password_digit = str((int(digit) - 3) % 10)  #modulo division is still needed so the returned pasword will not be negative
+        # add the new digit into the new decoded password
+        OG_password += password_digit
+    return OG_password
+
+
+def menu():
+    user_option = ""
+    encoded = ""
+    while user_option != "3":
+        print("Menu")
+        print("-------------")
+        print("1. Encode\n2. Decode\n3. Quit")
+        print("")
+        print("")
+
+        user_option = input("Please enter an option: ")
+        if user_option == "1":
+            user_password = input("Enter the password to encode: ")
+            encoded = encode_password(user_password)  # Store the encoded password
+            print("Your password has been encoded and stored!")
+            print("")
+            encode_password(user_password)
+        if user_option == "2":
+            decoded = decode_password(encoded)
+            print("The encoded password is " + encoded + " and the original password is " + decoded + ".")
+            print("")
+        if user_option == "3":
+            break
+
+
+
+
+
+menu()
